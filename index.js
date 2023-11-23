2c7dd26264ecf39ef927a68cc9f1707ede2661f1
bhuvneshwari <bhuvneshwarirathore4@gmail.com>
2023-11-23 19:16:41 +0530

    added other functionalities

--- index.js
+++ index.js
@@ -46,6 +46,30 @@ class UI {
 
         list.appendChild(row);
     }
+
+    static deleteBook(el){
+        if(el.classList.contains('delete')){
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message,className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div,form);
+
+        // Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(),3000);
+    }
+
+    static clearFields(){
+        document.querySelector('#title').value='';
+        document.querySelector('#author').value='';
+        document.querySelector('#isbn').value='';
+    }
 }
 
 
@@ -58,14 +82,42 @@ document.addEventListener('DOMContentLoaded',UI.displayBooks)
 
 // Event : Add a Book
 
-document.querySelector('#book-form').addEventListener('submit',(e)
-=> {
-    // Get from values
+document.querySelector('#book-form').addEventListener('submit',(e) => {
+
+    // Prevent actual submit
+    e.preventDefault();
+
+    // Get form values
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
+
+    // Validate
+    if(title === '' || author === '' || isbn === ''){
+        UI.showAlert('Please fill all fields','danger');
+
+    }
+    else{
+    // Instatiate book
+    const book = new Book(title,author,isbn);
+
+    // Add Book to UI
+    UI.addBookToList(book);
+
+    // Show success message
+    UI.showAlert('Book Added','SUCCESS')
+
+    // Clear fields
+    UI.clearFields();
+    }
 
 });
 
 
 // Event : Remove a Book
+
+document.querySelector('#book-list').addEventListener('click',(e) => {
+    UI.deleteBook(e.target);
+    // Show success message
+    UI.showAlert('Book Removed','SUCCESS')
+});
